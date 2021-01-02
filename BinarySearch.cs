ca60c889d0a861fac8fdbc6116d769cf37c3184d
Abdelrahman Tawfeek <Abdelrahman@ireckonu.com>
2021-01-02 17:17:19 +0100

    add-sln-binary-search

---
+++ BinarySearch.cs
@@ -0,0 +1,82 @@
+﻿namespace algorithm
+{
+    public static class BinarySearch
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="target"></param>
+        /// <param name="minIndex"></param>
+        /// <param name="maxIndex"></param>
+        /// <returns></returns>
+        public static int SearchUsingRecursion(int[] array, int target, int minIndex, int maxIndex)
+        {
+            if (array[minIndex] == target)
+                return minIndex;
+
+            if(array[maxIndex] == target)
+                return maxIndex;
+
+            if (minIndex == maxIndex - 1)
+                return -1;
+
+            int midIndex = (maxIndex + minIndex) / 2;
+
+            if (array[midIndex] == target)
+                return midIndex;
+
+            if (array[midIndex] > target)
+                return SearchUsingRecursion(array, target, minIndex, midIndex);
+            else            
+                return SearchUsingRecursion(array, target, midIndex, maxIndex);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int SearchUsingWhileLoop(int[] array, int target)
+        {
+            // Your code here
+            int minIndex = 0;
+            int maxIndex = array.Length - 1;
+
+            while (minIndex < maxIndex)
+            {
+                if (array[minIndex] == target)
+                    return minIndex;
+
+                if (array[maxIndex] == target)
+                    return maxIndex;
+
+                if (maxIndex == minIndex + 1)
+                    return -1;
+
+                int midIndex = (minIndex + maxIndex) / 2;
+
+                if (array[midIndex] == target)
+                {
+                    return midIndex;
+                }
+
+                if (array[midIndex] > target)
+                {
+                    maxIndex = midIndex - 1;
+                }
+
+                if (array[midIndex] < target)
+                {
+                    minIndex = midIndex + 1;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -3, 2, 4, 10, 12, 14, 17, 18, 20, 25, 36, 78, 100, 110 };    // (n) elements
+            int target = 78;
+
+            int targetIndex = BinarySearch.SearchUsingRecursion(array, target, 0, array.Length - 1);
+            Console.WriteLine($"result of binary search using recursion : {targetIndex}");
+
+            targetIndex = BinarySearch.SearchUsingWhileLoop(array, target);
+            Console.WriteLine($"result of binary search using while loop : {targetIndex}");
+        }
+    }
+}
